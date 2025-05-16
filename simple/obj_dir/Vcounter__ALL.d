482bd398b97de7978907734160baf35a25a08643
IAROSLAV VOITOVYCH <yaroslav.voytovych@gmail.com>
2025-05-17 01:01:34 +0200

    Simple Counter: Adding the intermediate files

---
+++ simple/obj_dir/Vcounter__ALL.d
@@ -0,0 +1,14 @@
+Vcounter__ALL.o: Vcounter__ALL.cpp Vcounter.cpp Vcounter__pch.h \
+ /workdir/verilator/include/verilated.h \
+ /workdir/verilator/include/verilatedos.h \
+ /workdir/verilator/include/verilated_config.h \
+ /workdir/verilator/include/verilated_types.h \
+ /workdir/verilator/include/verilated_funcs.h Vcounter__Syms.h Vcounter.h \
+ Vcounter___024root.h /workdir/verilator/include/verilated_vcd_c.h \
+ /workdir/verilator/include/verilated.h \
+ /workdir/verilator/include/verilated_trace.h \
+ Vcounter___024root__DepSet_ha8ec69a6__0.cpp \
+ Vcounter___024root__DepSet_he26a92aa__0.cpp Vcounter__Trace__0.cpp \
+ Vcounter___024root__Slow.cpp \
+ Vcounter___024root__DepSet_he26a92aa__0__Slow.cpp Vcounter__Syms.cpp \
+ Vcounter__Trace__0__Slow.cpp Vcounter__TraceDecls__0__Slow.cpp
---
+++ simple/obj_dir/sim_main.d
@@ -0,0 +1,6 @@
+sim_main.o: ../sim_main.cpp Vcounter.h \
+ /workdir/verilator/include/verilated.h \
+ /workdir/verilator/include/verilatedos.h \
+ /workdir/verilator/include/verilated_config.h \
+ /workdir/verilator/include/verilated_types.h \
+ /workdir/verilator/include/verilated_funcs.h
