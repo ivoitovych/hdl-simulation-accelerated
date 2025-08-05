acd09372494357c5a61299bce70bb43e151b3900
IAROSLAV VOITOVYCH <iaroslav_voitovych@epam.com>
2025-08-05 22:18:50 +0000

    Moved simple into examples directory for uniformity

--- simple/obj_dir/sim_main.d
+++
@@ -1,6 +0,0 @@
-sim_main.o: ../sim_main.cpp Vcounter.h \
- /workdir/verilator/include/verilated.h \
- /workdir/verilator/include/verilatedos.h \
- /workdir/verilator/include/verilated_config.h \
- /workdir/verilator/include/verilated_types.h \
- /workdir/verilator/include/verilated_funcs.h
